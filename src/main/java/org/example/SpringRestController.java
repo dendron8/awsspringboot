6736d7aeb4f750013da6fc5092cfb77e4aac4033
430014481 <430014481@gnwrala2061960.genworth.net>
2024-10-22 10:41:28 -0400

    Initial commit

---
+++ src/main/java/org/example/SpringRestController.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
+@RestController
+public class SpringRestController {
+
+    @GetMapping("/connect")
+    public String connect() {
+        System.out.println("connected to spring boot");
+        return "connected";
+    }
+}
