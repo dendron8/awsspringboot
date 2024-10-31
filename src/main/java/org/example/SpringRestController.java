2a3c24b70ddd0704ae267df0d2ae29082a95c7e2
dendron8 <phillip.t.helm@gmail.com>
2024-10-31 10:27:16 -0400

    result detail

--- src/main/java/org/example/SpringRestController.java
+++ src/main/java/org/example/SpringRestController.java
@@ -13,6 +13,10 @@ public class SpringRestController {
     @GetMapping("/connect")
     public String connect() {
         System.out.println("connected to spring boot");
-        return "connected!!!!!";
+        return "This page is the result of an application that was deployed with " +
+                "the following flow:" +
+                "1. code pushed to https://github.com/dendron8/awsspringboot" +
+                "2. CodeBuild job aws-spring-boot triggered automatically, runs gradle build and a docker build/push to ECR via buildspec.yml file." +
+                "3. an event via EventBridge is triggered and sent to an ECS cluster to deploy the new code.";
     }
 }
