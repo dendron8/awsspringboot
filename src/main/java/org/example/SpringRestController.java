500a810df31ceca36e9d45eca58337db68410f5e
dendron8 <phillip.t.helm@gmail.com>
2025-01-19 21:51:46 -0500

    update rest controller

--- src/main/java/org/example/SpringRestController.java
+++ src/main/java/org/example/SpringRestController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
@@ -14,14 +15,22 @@ public class SpringRestController {
 
     private static final Logger log = LoggerFactory.getLogger(SpringRestController.class);
 
+    private final ObservableUserService ouService;
+
+    SpringRestController(ObservableUserService ouService) {
+        this.ouService = ouService;
+    }
+
+    @GetMapping("/user/{userId}")
+    String userName(@PathVariable("userId") String userId) {
+        log.info("Got a request");
+        return ouService.userName(userId);
+    }
+
     @GetMapping("/connect")
     public String connect() {
         log.info("got in connect");
         System.out.println("connected to spring boot!");
-        return "This page is the result of an application that was deployed with " +
-                "the following flow:" +
-                "1. code pushed to https://github.com/dendron8/awsspringboot" +
-                "2. CodeBuild job aws-spring-boot triggered automatically, runs gradle build and a docker build/push to ECR via buildspec.yml file." +
-                "3. an event via EventBridge is triggered and sent to an ECS cluster to deploy the new code.";
+        return "";
     }
 }
